2060228c321fa73178ef4f3c0fafe78134529995
Adrian <adrian.amoke@gmail.com>
2025-03-06 12:13:48 +0300

    ed

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function logWhisper(string) {
     console.log(string.toLowerCase());
 }
 function sayHiToHeadphonedRoommate(string) {
-    if (string === string.toLowerCase) {
-        return "I can\'t hear you!";
+    if (string.toLowerCase(string) === string) {
+        return "I can't hear you!";
     }
-    else if (string === string.toUpperCase) {
+    else if (string.toUpperCase(string) === string) {
         return "YES INDEED!";
     }
-    else if (string === "Let\'s have dinner together!") {
+    else if (string === "Let's have dinner together!") {
         return 'I would love to!';
     }
-}    
+}
